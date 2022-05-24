9771f363a2046bd9ae85e7c96324660d56fd60d0
Rinasham <rinashamau@gmail.com>
2022-05-24 14:50:26 +1000

    [add] map

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./note.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
